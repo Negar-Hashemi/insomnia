a5712d3e54b4e0c3b78d367245065d7db78fabbd
Kent Wang <kent.wang@konghq.com>
2024-12-16 14:14:03 +0800

    1.use service account key file path instead of saving file content for gcp

--- packages/insomnia/src/main/ipc/cloud-service-integraion/cloud-service.ts
+++ packages/insomnia/src/main/ipc/cloud-service-integraion/cloud-service.ts
@@ -1,5 +1,3 @@
-import type { JWTInput } from 'google-auth-library';
-
 import * as models from '../../../models';
 import type { AWSTemporaryCredential, BaseCloudCredential, CloudProviderName } from '../../../models/cloud-credential';
 import { ipcMainHandle, ipcMainOn } from '../electron';
@@ -45,7 +43,7 @@ class ServiceFactory {
       case 'aws':
         return new AWSService(credential as AWSTemporaryCredential);
       case 'gcp':
-        return new GCPService(credential as JWTInput);
+        return new GCPService(credential as string);
       default:
         throw new Error('Invalid cloud service provider name');
     }
