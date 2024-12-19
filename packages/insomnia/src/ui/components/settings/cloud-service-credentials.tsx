7c995720d1a0b29c263667835dad091b833dc7ef
Kent Wang <kent.wang@konghq.com>
2024-12-19 15:22:52 +0800

    1.update the gcp & aws logo

--- packages/insomnia/src/ui/components/settings/cloud-service-credentials.tsx
+++ packages/insomnia/src/ui/components/settings/cloud-service-credentials.tsx
@@ -9,6 +9,7 @@ import { Icon } from '../icon';
 import { showModal } from '../modals';
 import { AskModal } from '../modals/ask-modal';
 import { CloudCredentialModal } from '../modals/cloud-credential-modal/cloud-credential-modal';
+import { SvgIcon } from '../svg-icon';
 import { UpgradeNotice } from '../upgrade-notice';
 import { NumberSetting } from './number-setting';
 
@@ -26,7 +27,7 @@ const createCredentialItemList: createCredentialItemType[] = [
   {
     id: 'gcp',
     name: getProviderDisplayName('gcp'),
-    icon: <i className="ml-1 fa-brands fa-google" />,
+    icon: <SvgIcon icon='gcp-logo' className='ml-1' />,
   },
 ];
 const buttonClassName = 'disabled:opacity-50 h-7 aspect-square aria-pressed:bg-[--hl-sm] rounded-sm text-[--color-font] hover:bg-[--hl-xs] transition-all text-sm py-1 px-2';
